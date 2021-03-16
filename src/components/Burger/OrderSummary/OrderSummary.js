9fedaa9f27740019c1db74d5f34144b1919be7a7
echoLive <superguy0613519@gmail.com>
2021-03-16 14:47:19 -0700

    Changing the Folder Structure(156)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
  import React, { Component } from 'react';
 
- import Aux from '../../../hoc/Aux';
+ import Aux from '../../../hoc/Aux/Aux';
  import Button from '../../UI/Button/Button';
 
  class OrderSummary extends Component {
