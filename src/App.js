e53c6b8e962aae883bfa0f6155bce1b100008e54
echoLive <superguy0613519@gmail.com>
2021-03-25 10:07:18 +0100

    Building the Checkout Container(221)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
     componentDidMount () {
@@ -16,6 +17,7 @@ class App extends Component {
             <div>
                 <Layout>
                     <BurgerBuilder />
+                    <Checkout />
                 </Layout>
             </div>
         );
