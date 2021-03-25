a090ffecc9d4d376719e1a0d6ee1c93c9fb2c93b
echoLive <superguy0613519@gmail.com>
2021-03-25 10:36:58 +0100

    Navigationg Back & To Page(224)

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -13,11 +13,11 @@ const checkoutSummary = (props) => {
             </div>
             <Button
                 btnType="Danger"
-                clicked
+                clicked={props.checkoutCancelled}
                 >CANCEL</Button>
             <Button
                 btnType="Success"
-                clicked
+                clicked={props.checkoutContinued}
                 >CONTINUE</Button>
         </div>
     )
